9128cf7b052e7bbe49849cfde1f8e02eaede6297
eojunhwang <eojun23@gmail.com>
2021-01-13 16:17:55 +0900

    복구

--- src/main/java/com/arendelle/book/springboot/web/IndexController.java
+++ src/main/java/com/arendelle/book/springboot/web/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -26,8 +27,8 @@ public class IndexController {
         return "posts-save";
     }
 
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    @GetMapping("/posts/update")
+    public String postsUpdate(@RequestParam Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
